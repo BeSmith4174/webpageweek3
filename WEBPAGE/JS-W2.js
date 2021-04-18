a899f32ebb7844918e7fd4d2a79cdb4406d11058
BeSmith4174 <82059759+BeSmith4174@users.noreply.github.com>
2021-04-18 17:31:21 -0500

    completed page with notes

--- WEBPAGE/JS-W2.js
+++ WEBPAGE/JS-W2.js
@@ -1,20 +1,46 @@
+//same function but made more efficent with loops, included is change order request.
+function countDownV3() {
+    var count = 10;
+
+    for (var i = 1; i <= 11; i++) {
+
+        if (i <= 5) {
+            setTimeout(function () {
+                document.getElementById("countDownTimer").innerHTML = count;
+                count--;
+            }, 1000 * i);
+        } else if (i > 5 && i != 11) {
+            setTimeout(function () {
+                document.getElementById("countDownTimer").innerHTML = "warning less than half way to launch,timeleft =" + count;
+                count--;
+            }, 1000 * i);
+        } else {
+            setTimeout(function () {
+                document.getElementById("countDownTimer").innerHTML = "Blastoff!";
+                count--;
+            }, 1000 * i);
+        }
+    }
+}
+
+
 //same function but made more efficent with loops
 function countDownV2() {
     var count = 10;
-//this sets how my count will be between 1 and 10
+    //this sets how my count will be between 1 and 10
     for (var i = 1; i <= 10; i++) {
         setTimeout(function () {
             //this identifies 10 as my starting point
             document.getElementById("countDownTimer").innerHTML = count;
             count--;
-//numbercount represented in miliseconds
+            //numbercount represented in miliseconds
         }, 1000 * i);
     }
     setTimeout(function () {
         //this identifies Blastoff as my stopping point
         document.getElementById("countDownTimer").innerHTML = "Blastoff!";
         count--;
-//numbercount represented in miliseconds
+        //numbercount represented in miliseconds
     }, 11000 * i);
 
 
@@ -100,37 +126,37 @@ function countDown() {
     }, 11000);
 }
 
-function playCraps(){
-//create die1 variable
-var die1;
-//create die2 variable
-var die2;
-//create the varience of the sum of 2 variables
-var sum;
-// rounds random generated number for die1 to the nearest whole number
-die1 = Math.ceil(Math.random() * 6);
-// rounds random generated number for die2 to the nearest whole number
-die2 = Math.ceil(Math.random() * 6);
-// shows that die1 and die2 will be used to determine the sum 
-sum = die1 + die2;
-// this line takes the number rolled for dice1 and displays it  
-document.getElementById("die1Res").innerHTML = die1;
-// this line takes the number rolled for dice2 and displays it 
-document.getElementById("die2Res").innerHTML = die2;
-// determines the sum of both die and displays it
-document.getElementById("sumRes").innerHTML = sum;
+function playCraps() {
+    //create die1 variable
+    var die1;
+    //create die2 variable
+    var die2;
+    //create the varience of the sum of 2 variables
+    var sum;
+    // rounds random generated number for die1 to the nearest whole number
+    die1 = Math.ceil(Math.random() * 6);
+    // rounds random generated number for die2 to the nearest whole number
+    die2 = Math.ceil(Math.random() * 6);
+    // shows that die1 and die2 will be used to determine the sum 
+    sum = die1 + die2;
+    // this line takes the number rolled for dice1 and displays it  
+    document.getElementById("die1Res").innerHTML = die1;
+    // this line takes the number rolled for dice2 and displays it 
+    document.getElementById("die2Res").innerHTML = die2;
+    // determines the sum of both die and displays it
+    document.getElementById("sumRes").innerHTML = sum;
 
-// this line sets the parameters for what numbers cause a loss
-if (sum == 7 || sum == 11) {
-    document.getElementById("crapsResults").innerHTML = "Craps! You Loose!";
-}
-// this line sets parameters on what numbers win
-else if (die1 == die2 && die1 % 2 == 0) {
-    document.getElementById("crapsResults").innerHTML = "Doubles, you win!!";
-    // this line tells the user to reroll 
-} else {
-    document.getElementById("crapsResults").innerHTML = "Push. Please roll again.";
-}
+    // this line sets the parameters for what numbers cause a loss
+    if (sum == 7 || sum == 11) {
+        document.getElementById("crapsResults").innerHTML = "Craps! You Loose!";
+    }
+    // this line sets parameters on what numbers win
+    else if (die1 == die2 && die1 % 2 == 0) {
+        document.getElementById("crapsResults").innerHTML = "Doubles, you win!!";
+        // this line tells the user to reroll 
+    } else {
+        document.getElementById("crapsResults").innerHTML = "Push. Please roll again.";
+    }
 
 
 
--- WEBPAGE/Loginwk3.js
+++ WEBPAGE/Loginwk3.js
@@ -5,22 +5,22 @@ function checkCreds() {
     var lastName = document.getElementById("lName").value;
     //place to store badge id
     var badgeNumb = document.getElementById("badgeID").value;
-
+    // displays first and last name with a space between them
     var fullName = firstName + " " + lastName;
-
+    //function that throw error if too many characters are entered
     if (fullName.length > 20 || fullName == 1) {
 
         document.getElementById("loginStatus").innerHTML = "Invalid full name!";
     }
-
+    //throw an error if to many numbers are used for badge number
     if (badgeNumb > 999 || badgeNumb < 1) {
         document.getElementById("loginStatus").innerHTML = "Invalid badge number!";
 
     }
-
-    if(fullName.length<21 && fullName.length > 1 && badgeNumb < 1000 && badgeNumb > 0 ){
-alert("Access Granted! Welcome! " + fullName);
-location.replace("HTMLW2.html")
+    //displays welcome with full name once all parameters set
+    if (fullName.length < 21 && fullName.length > 1 && badgeNumb < 1000 && badgeNumb > 0) {
+        alert("Access Granted! Welcome! " + fullName);
+        location.replace("HTMLW2.html")
     }
 
 
